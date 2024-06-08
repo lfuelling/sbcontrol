f4f5f02091123d911a167c210b4a195df10e325c
Lukas Fülling <lukas@k40s.net>
2024-06-08 13:56:40 +0200

    Initial Commit

---
+++ sbcontrol/sbcontrolApp.swift
@@ -0,0 +1,17 @@
+//
+//  sbcontrolApp.swift
+//  sbcontrol
+//
+//  Created by Lukas Fülling on 6/8/24.
+//
+
+import SwiftUI
+
+@main
+struct sbcontrolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
