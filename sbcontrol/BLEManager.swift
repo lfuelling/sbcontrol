98718f0880b9697cce508207d5db84d5f5a2467a
Lukas Fülling <lukas@k40s.net>
2024-06-08 22:00:52 +0200

    add initial working version (volcano only)

---
+++ sbcontrol/BLEManager.swift
@@ -0,0 +1,104 @@
+//
+//  BLEManager.swift
+//  sbcontrol
+//
+//  Created by Lukas Fülling on 6/8/24.
+//
+
+import Foundation
+import CoreBluetooth
+
+class BLEManager: NSObject, ObservableObject, CBCentralManagerDelegate, CBPeripheralDelegate {
+    var centralManager: CBCentralManager!
+    var peripheral: CBPeripheral!
+    
+    @Published var peripherals: [CBPeripheral] = []
+    
+    @Published var selectedTemperature = -1
+    @Published var currentTemperature = -1
+    @Published var airStatue = false
+    @Published var heatStatue = false
+    
+    override init() {
+        super.init()
+        centralManager = CBCentralManager(delegate: self, queue: nil)
+        centralManager.delegate = self
+    }
+    
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+        } else {
+            print("Bluetooth not available.")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if !self.peripherals.contains(peripheral) && peripheral.name != nil {
+            self.peripherals.append(peripheral)
+        }
+    }
+    
+    func connectDevice(peripheral: CBPeripheral) {
+        self.centralManager.stopScan()
+        self.peripheral = peripheral
+        self.peripheral.delegate = self
+        self.centralManager.connect(self.peripheral, options: nil)
+    }
+    
+    func centralManager(_ central: CBCentralManager, didConnect peripheral: CBPeripheral) {
+        self.peripheral.discoverServices(nil)
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverServices error: Error?) {
+        for service in peripheral.services! {
+            self.peripheral.discoverCharacteristics(nil, for: service)
+        }
+    }
+    
+    func peripheral(_ peripheral: CBPeripheral, didDiscoverCharacteristicsFor service: CBService, error: Error?) {
+        if let characteristics = service.characteristics {
+            for characteristic in characteristics {
+                switch characteristic.uuid.uuidString.lowercased() {
+                case "10110001-5354-4f52-5a26-4249434b454c", // Current temperature
+                     "10110003-5354-4f52-5a26-4249434b454c", // Set temperature
+                    "1010000c-5354-4f52-5a26-4249434b454c": // stat1
+                    peripheral.setNotifyValue(true, for: characteristic)
+                    peripheral.readValue(for: characteristic)
+                default:
+                    break
+                }
+            }
+        }
+    }
+
+    func peripheral(_ peripheral: CBPeripheral, didUpdateValueFor characteristic: CBCharacteristic, error: Error?) {
+        if let value = characteristic.value {
+            switch characteristic.uuid.uuidString.lowercased() {
+            case "10110001-5354-4f52-5a26-4249434b454c": // Current temperature
+                let intValue = UInt32(littleEndian: value.withUnsafeBytes { $0.load(as: UInt32.self) })
+                let currentTemperature = intValue / 10
+                self.currentTemperature = Int(currentTemperature)
+                print("Current temperature: \(currentTemperature)")
+            case "10110003-5354-4f52-5a26-4249434b454c": // Set temperature
+                let intValue = UInt32(littleEndian: value.withUnsafeBytes { $0.load(as: UInt32.self) })
+                let setTemperature = intValue / 10
+                self.selectedTemperature = Int(setTemperature)
+                print("Set temperature: \(setTemperature)")
+            case "1010000c-5354-4f52-5a26-4249434b454c": // stat1
+                let intValue = Int(value[0])
+                let heaterStatus = (intValue & 0x0020) != 0
+                print("Heater status: \(heaterStatus ? "ON" : "OFF")")
+                self.heatStatue = heaterStatus
+                
+                let airPumpStatus = (intValue & 0x2000) != 0
+                print("Air pump status: \(airPumpStatus ? "ON" : "OFF")")
+                self.airStatue = airPumpStatus
+            default:
+                break
+            }
+        }
+    }
+    
+    // Implement other delegate methods as needed
+}
--- sbcontrol/ContentView.swift
+++ sbcontrol/ContentView.swift
@@ -6,16 +6,51 @@
 //
 
 import SwiftUI
+import CoreBluetooth
 
 struct ContentView: View {
+    @ObservedObject var bleManager = BLEManager()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        if bleManager.peripheral == nil {
+            VStack {
+                Text("BLE Devices")
+                List(bleManager.peripherals, id: \.identifier) { peripheral in
+                    HStack {
+                        Text(peripheral.name ?? "Unnamed")
+                        Spacer()
+                        Button(action: {
+                            self.bleManager.connectDevice(peripheral: peripheral)
+                        }) {
+                            Text("Connect")
+                        }
+                    }
+                }
+            }
+        } else {
+            VStack {
+                Text(bleManager.peripheral.name ??  "Unnamed")
+                    .font(.largeTitle)
+                Spacer()
+                Text("Current: \(bleManager.currentTemperature)°C")
+                    .bold()
+                    .font(.title)
+                Text("Selected: \(bleManager.selectedTemperature)°C")
+                    .font(.title)
+                HStack {
+                    Spacer()
+                    Image(systemName: bleManager.heatStatue ? "thermometer.high" : "thermometer.medium.slash")
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                    Spacer()
+                    Image(systemName: bleManager.airStatue ? "humidifier.and.droplets.fill" : "humidifier.and.droplets")
+                        .resizable()
+                        .frame(width: 32, height: 32)
+                    Spacer()
+                }
+                Spacer()
+            }
         }
-        .padding()
     }
 }
 
