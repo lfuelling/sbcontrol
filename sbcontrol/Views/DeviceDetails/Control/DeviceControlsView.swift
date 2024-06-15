e9eee04d4b881b420e8433608a83ecdf13fb56fb
Lukas Fülling <lukas@k40s.net>
2024-06-15 16:29:46 +0200

    hide air button when air is unavailable

--- sbcontrol/Views/DeviceDetails/Control/DeviceControlsView.swift
+++ sbcontrol/Views/DeviceDetails/Control/DeviceControlsView.swift
@@ -12,22 +12,32 @@ struct DeviceControlsView: View {
     
     var body: some View {
 #if os(macOS)
-        HStack {
-            DeviceControlsHeatButton()
-            Spacer()
-            VStack(spacing: 2) {
+        if(bleManager.deviceDetermination.hasAir) {
+            HStack {
+                DeviceControlsHeatButton()
+                Spacer()
+                VStack(spacing: 2) {
+                    DeviceControlsBatterySection()
+                    DeviceControlsTemperatureSection()
+                }
+                Spacer()
+                DeviceControlsAirButton()
+            }.padding()
+        } else {
+            VStack {
                 DeviceControlsBatterySection()
+                DeviceControlsHeatButton()
                 DeviceControlsTemperatureSection()
-            }
-            Spacer()
-            DeviceControlsAirButton()
-        }.padding()
+            }.padding()
+        }
 #else
         VStack {
             DeviceControlsBatterySection()
             DeviceControlsTemperatureSection()
             DeviceControlsHeatButton()
-            DeviceControlsAirButton()
+            if(bleManager.deviceDetermination.hasAir) {
+                DeviceControlsAirButton()
+            }
         }.padding()
 #endif
     }
