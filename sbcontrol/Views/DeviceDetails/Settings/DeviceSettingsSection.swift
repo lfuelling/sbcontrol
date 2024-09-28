ac4f529754212d5558a6d87778aa46bc21d330ff
Lukas Fülling <lukas@k40s.net>
2024-09-28 20:24:20 +0200

    hide settings section completely when no settings are present

--- sbcontrol/Views/DeviceDetails/Settings/DeviceSettingsSection.swift
+++ sbcontrol/Views/DeviceDetails/Settings/DeviceSettingsSection.swift
@@ -14,38 +14,41 @@ struct DeviceSettingsSection: View {
     @State private var autoShutoffTime: Double = -1 // TODO: debounce/throttle
     
     var body: some View {
-        Section {
-            if(deviceState.deviceDetermination.hasScreen) {
-                VStack(alignment: .leading) {
-                    Text("LED Brightness (\(Int(deviceLEDBrightness)))")
-                    Slider(value: $deviceLEDBrightness, in: 1...100).onAppear {
-                        deviceLEDBrightness = Double(deviceState.deviceLEDBrightness)
-                    }.onChange(of: deviceLEDBrightness) {
-                        if(Int(deviceLEDBrightness) != deviceState.deviceLEDBrightness) {
-                            let _ = deviceState.setLEDBrightness(Int(deviceLEDBrightness))
+        if(deviceState.deviceDetermination.hasScreen ||
+           deviceState.deviceDetermination.hasAutoshutoffTime) {
+            Section {
+                if(deviceState.deviceDetermination.hasScreen) {
+                    VStack(alignment: .leading) {
+                        Text("LED Brightness (\(Int(deviceLEDBrightness)))")
+                        Slider(value: $deviceLEDBrightness, in: 1...100).onAppear {
+                            deviceLEDBrightness = Double(deviceState.deviceLEDBrightness)
+                        }.onChange(of: deviceLEDBrightness) {
+                            if(Int(deviceLEDBrightness) != deviceState.deviceLEDBrightness) {
+                                let _ = deviceState.setLEDBrightness(Int(deviceLEDBrightness))
+                            }
+                        }.onChange(of: deviceState.deviceLEDBrightness) {
+                            deviceLEDBrightness = Double(deviceState.deviceLEDBrightness)
                         }
-                    }.onChange(of: deviceState.deviceLEDBrightness) {
-                        deviceLEDBrightness = Double(deviceState.deviceLEDBrightness)
-                    }
-                }.padding(.vertical, 4)
-            }
-            
-            if(deviceState.deviceDetermination.hasAutoshutoffTime) {
-                VStack(alignment: .leading) {
-                    Text("Auto-Shutoff Time (\(Int(autoShutoffTime / 60)) min)")
-                    Slider(value: $autoShutoffTime, in: 1800...21600).onAppear {
-                        autoShutoffTime = Double(deviceState.deviceAutoShutoffTime)
-                    }.onChange(of: autoShutoffTime) {
-                        if(Int(autoShutoffTime) != deviceState.deviceAutoShutoffTime) {
-                            let _ = deviceState.setAutoShutoffTime(Int(autoShutoffTime))
+                    }.padding(.vertical, 4)
+                }
+                
+                if(deviceState.deviceDetermination.hasAutoshutoffTime) {
+                    VStack(alignment: .leading) {
+                        Text("Auto-Shutoff Time (\(Int(autoShutoffTime / 60)) min)")
+                        Slider(value: $autoShutoffTime, in: 1800...21600).onAppear {
+                            autoShutoffTime = Double(deviceState.deviceAutoShutoffTime)
+                        }.onChange(of: autoShutoffTime) {
+                            if(Int(autoShutoffTime) != deviceState.deviceAutoShutoffTime) {
+                                let _ = deviceState.setAutoShutoffTime(Int(autoShutoffTime))
+                            }
+                        }.onChange(of: deviceState.deviceAutoShutoffTime) {
+                            autoShutoffTime = Double(deviceState.deviceAutoShutoffTime)
                         }
-                    }.onChange(of: deviceState.deviceAutoShutoffTime) {
-                        autoShutoffTime = Double(deviceState.deviceAutoShutoffTime)
-                    }
-                }.padding(.vertical, 4)
+                    }.padding(.vertical, 4)
+                }
+            } header: {
+                Text("Device Settings")
             }
-        } header: {
-            Text("Device Settings")
         }
     }
 }
